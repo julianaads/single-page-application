bf97982d926529eb1a4779e57b1992eb785673c0
Juliana <juliana.souzas@yahoo.com.br>
2021-04-19 19:45:17 -0300

    conteúdo about

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -2,10 +2,10 @@ export default () =>{
     const container = document.createElement('div');
 
     const template = `
-    <h2> Sobre o Anime <h2>
-    <section>
-    <div>
-    <p>Naruto (ナルト?) é uma série de mangá escrita e ilustrada por Masashi Kishimoto, 
+    <h2 class="titulo-principal-about"> Sobre o Anime <h2>
+    <section class="container-about">
+    <div class="container-about-secundario">
+    <p> Naruto (ナルト?) é uma série de mangá escrita e ilustrada por Masashi Kishimoto, 
     que conta a história de Naruto Uzumaki, um jovem ninja que constantemente procura por reconhecimento e 
     sonha em se tornar Hokage, o ninja líder de sua vila. A história é dividida em duas partes, a primeira parte 
     se passa nos anos da pré-adolescência de Naruto, e a segunda parte se passa em sua adolescência. 
@@ -33,32 +33,45 @@ export default () =>{
     observaram que o mangá, que tem uma história coming-of-age, faz uso de referências culturais da mitologia 
     japonesa e do confucionismo. </p>
     </div>
-<figure><img src="https://media.giphy.com/media/ohT97gdpR40vK/source.gif"></figure>
 
-<h1>Enredo</h1>
-<div>
-<h2>Parte I</h2>
+<figure class="img-about-section">
+<img class="img-about" src="https://media.giphy.com/media/ohT97gdpR40vK/source.gif">
+</figure>
+
+<h2 class="subtitulo-principal-about">Enredo</h2>
+
+<h2 class="subtitulo-about">Parte I</h2>
+
+<div class="container-about-secundario">
+
 <p>
 A história principal se foca em Naruto e seu desenvolvimento quanto ninja, junto com os seus amigos. 
 Também se centra nas interações entre estes e a influência do ambiente em suas personalidades. Conforme 
 transcorre a série, Naruto se relaciona com Sasuke Uchiha e Sakura Haruno, com quem forma o “Time 7”, 
-juntamente com o sensei Kakashi Hatake.[3] Cabe mencionar que Naruto confia muito neles assim como em em outros 
+juntamente com o sensei Kakashi Hatake. Cabe mencionar que Naruto confia muito neles assim como em em outros 
 personagens que irá conhecendo mais adiante. Enquanto eles aprendem novas habilidades e conhecem novas pessoas 
 e lugares em suas missões, Naruto luta por seu sonho de se tornar o líder máximo de sua aldeia (Hokage) e ser 
-reconhecido como alguém importante.[4] No início, a série se enfoca nos integrantes do Time 7, Naruto, Sasuke e Sakura.
-Pouco depois, Orochimaru (um dos vilões mais procurados)[7] ataca a Aldeia Oculta da Folha, assassinando o 
-Terceiro Hokage em um ato de vingança pessoal.[8] Isso acaba desencadeando que Jiraiya, um dos três ninjas 
-legendários (Sannins),[9] inicie a busca da sua antiga companheira de equipe Tsunade para designá-la como a 
-Quinta Hokage.[10] Durante a sua busca é revelado que Orochimaru quer encontrar Sasuke 
-(a quem conhece por suas técnicas de dōjutsu)[11] para oferecer-lhe o poder que tanto deseja para matar seu 
-irmão Itachi Uchiha,[12] responsável pelo assassinato de todo seu clã.[13][14] Sasuke aceita a proposta de 
-Orochimaru e vai treinar com ele, traindo a sua aldeia.[15] Enquanto isso, Naruto decide fazer algo a respeito, 
+reconhecido como alguém importante. No início, a série se enfoca nos integrantes do Time 7, Naruto, Sasuke e Sakura.
+Pouco depois, Orochimaru (um dos vilões mais procurados) ataca a Aldeia Oculta da Folha, assassinando o 
+Terceiro Hokage em um ato de vingança pessoal. Isso acaba desencadeando que Jiraiya, um dos três ninjas 
+legendários (Sannins), inicie a busca da sua antiga companheira de equipe Tsunade para designá-la como a 
+Quinta Hokage. Durante a sua busca é revelado que Orochimaru quer encontrar Sasuke 
+(a quem conhece por suas técnicas de dōjutsu) para oferecer-lhe o poder que tanto deseja para matar seu 
+irmão Itachi Uchiha, responsável pelo assassinato de todo seu clã. Sasuke aceita a proposta de 
+Orochimaru e vai treinar com ele, traindo a sua aldeia. Enquanto isso, Naruto decide fazer algo a respeito, 
 e então resolve deixar a aldeia junto com Jiraiya durante dois anos e meio com o objetivo de treinar e 
 preparar-se para a próxima vez que encontrar Sasuke, a quem tentará salvar.
 </p>
-<figure><img src="https://media.giphy.com/media/10SZwUqnHDyKqY/giphy.gif"></figure>
+
 </div>
-<h2>Parte 2 - Naruto Shippuden</h2>
+
+<figure class="img-about-section">
+<img class="img-about" src="https://media.giphy.com/media/10SZwUqnHDyKqY/giphy.gif">
+</figure>
+
+<h2 class="subtitulo-about">Parte 2 - Naruto Shippuden</h2>
+
+<div class="container-about-secundario">
 <p>
 Ao concluir o período de treinamento, Naruto regressa a aldeia justo quando uma misteriosa organização 
 denominada Akatsuki está tentando capturar as nove poderosas bestas Bijuus, incluindo a 
@@ -80,7 +93,13 @@ Entristecido por isso, Sasuke ingressa nas forças da Akatsuki com o objetivo de
  que consiste em criar uma ilusão a nível global, suficientemente poderosa parta controlar a humanidade, através 
  da lua. Diante disso, todos os líderes das cinco vilas ninjas, recusando lhe apoiar, se unem para 
  confrontá-lo; por isso, decidem esconder os últimos Jinchūrikis dentro de uma ilha secreta no País do Trovão.
- <figure><img src="https://media.giphy.com/media/hxXhy9z4x6N2w/giphy.gif"></figure>
+</div>
+
+ <figure class="img-about-section">
+ <img class="img-about" src="https://media.giphy.com/media/hxXhy9z4x6N2w/giphy.gif">
+ </figure>
+
+ <div class="container-about-secundario">
  Ali, Naruto conhece Killer Bee, um shinobi que contém a Gyuuki dentro dele, e Naruto pede ajuda para que o 
  treine para que possa controlar de forma correta o chakra da Raposa de Nove Caudas. Depois de um árduo 
  treinamento, Naruto obtêm com êxito o controle do poder da raposa, com a ajuda do espírito de sua mãe, 
@@ -88,21 +107,25 @@ Entristecido por isso, Sasuke ingressa nas forças da Akatsuki com o objetivo de
  sobre o lugar onde se escondem as últimas bestas; este se suicida, mas consegue mandar a mensagem com êxito. 
  Enquanto isso Madara vai em busca dos olhos de Nagato, matando Konan no processo, e manda Kabuto capturar os 
  dois ninjas restantes.
+</div>
+ <figure class="img-about-section">
+ <img class="img-about" src="https://media.giphy.com/media/xbgTlkPxbZaa4/giphy.gif">
+ </figure>
 
- <figure><img src="https://media.giphy.com/media/xbgTlkPxbZaa4/giphy.gif"></figure>
+ <div class="container-about-secundario">
 Graças ao conhecimento de Orochimaru, Kabuto utiliza um jutsu proibido e revive ninjas famosos para os 
-utilizar na guerra que se aproxima, enquanto que a Aliança Shinobi se prepara de igual maneira.[41] 
+utilizar na guerra que se aproxima, enquanto que a Aliança Shinobi se prepara de igual maneira. 
 Dentre os mortos ressuscitados, estão Os Irmãos de Ouro e Prata, Ginkaku e Kinkaku, que possuem parte do chakra 
-da Raposa de Nove Caudas, e devido a isto Naruto fica sabendo do que está se sucedendo.[42] Uma vez que se da 
+da Raposa de Nove Caudas, e devido a isto Naruto fica sabendo do que está se sucedendo. Uma vez que se da 
 conta da guerra, Naruto foge com Bee e em seu caminho se encontra com Tsunade e o Raikage; este último tenta 
 persuadi-los para que não se entrometam na batalha, mas Naruto se nega e finalmente o convence a deixá-los ir 
-combater a Akatsuki.[43] Os dois Jinchūrikis, com a ajuda de vários clones das sombras de Naruto, combatem um 
+combater a Akatsuki. Os dois Jinchūrikis, com a ajuda de vários clones das sombras de Naruto, combatem um 
 exército de Zetsus, seres criados a partir do DNA do primeiro Hokage, o que dá vantagem para as forças da 
 Aliança; Gaara e os demais shinobis confrontam os ninjas revividos pelo Edo Tensei. Depois disso, Itachi e 
 Nagato se encontram com Naruto e Bee, e começam a lutar; o Uchiha consegue libertar-se do controle de Kabuto 
-sobre a técnica de ressurreição e ajuda os outros a derrotar Nagato.[44] Pouco depois, Kabuto faz com que o 
+sobre a técnica de ressurreição e ajuda os outros a derrotar Nagato. Pouco depois, Kabuto faz com que o 
 segundo Tsuchikage reviva o seu mais poderoso guerreiro, Madara Uchiha; isto dá a entender que o verdadeiro 
-líder da Akatuki, Tobi, usou na realidade esse nome com o fim de intimidar os seus adversários.[45] Ōnoki – o 
+líder da Akatuki, Tobi, usou na realidade esse nome com o fim de intimidar os seus adversários. Ōnoki – o 
 atual Tsuchikage – e Gaara se veem encurralados pela força de Madara, mas os Kages restantes chegam para 
 equilibrar a situação. Com a ajuda do rin'negan, Tobi cria uma armadilha para Naruto e Bee, já que tem sob o 
 seu comando os cadáveres dos antigos Jinchūrikis com suas respectivas bestas seladas neles. A batalha contra 
@@ -129,7 +152,12 @@ resto do exército ninja chega para apoiar Naruto, e nesse momento Madara e Obit
  juntos tem um filho, Boruto Uzumaki, e uma filha, Himawari Uzumaki, enquanto Sasuke se casa com Sakura e juntos 
  tem uma filha, Sarada Uchiha. 
 </p>
-<figure><img src="https://s.aficionados.com.br/imagens/aberturas-de-naruto_w.jpg"></figure>
+</div>
+
+<figure class="img-about-section">
+<img class="img-about" src="https://s.aficionados.com.br/imagens/aberturas-de-naruto_w.jpg">
+</figure>
+
 </section>
     `;
 
